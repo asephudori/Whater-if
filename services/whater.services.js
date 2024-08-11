44c9d7ff05cffbae7d8542b48267d1b08fb3a46d
asep hudori <asep.hudori532@gmail.com>
2024-08-11 12:58:28 +0700

    add services file

---
+++ services/whater.services.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const { openMeteoBaseUrl } = require('../config');
+
+module.exports = {
+  getWather: async (latitude, longitude) => {
+    const url = `${openMeteoBaseUrl}?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      throw error;
+    }
+  },
+};
